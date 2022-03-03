79edb7aad192e8e52db313611d905b34afffd3e2
Dillon Ellis <dillonzellis@gmail.com>
2022-03-02 20:08:05 -0500

    Add effect hook to app to render products

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
+import { useEffect, useState } from "react";
+
 import Header from "./Header/Header";
 import CountButton from "./CountButton/CountButton";
 import SearchBar from "./SearchBar/SearchBar";
 
 import "./App.css";
 
-const products = ["David", "Jack", "John", "Joy", "Venom"];
+const App = () => {
+  const [productsState, setProductsState] = useState([]);
 
-function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      setProductsState(["David", "Jack", "John", "Joy", "Venom"]);
+    }, 2000);
+  });
+
+  const hasProducts = productsState.length > 0;
+
   return (
     <div className="App">
       <Header />
       <CountButton incrementBy={1} />
       <CountButton incrementBy={5} />
-      <SearchBar products={products} />
+      {hasProducts ? <SearchBar products={productsState} /> : "Loading..."}
     </div>
   );
-}
+};
 
 export default App;
